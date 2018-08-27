96e05ff2673f3cfee9123c977060f06effa8fcaa
Frannx1 <fsanguineti@itba.edu.ar>
2018-08-27 19:03:26 -0300

    run.sh running

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MediaTypeValidator
 A media-type validator given a media-range using a fsm
+
+
+# Instructions:
+-	Run make
--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-xterm -title "App 1" -hold -e ./src/main.out   &
+
+echo 'Insert the accepted media types (media-type1,mediatype2,...):'
+
+read var
+
+program='./src/a.out '$var
+
+eval $program
